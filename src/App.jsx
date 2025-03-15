791effec5f1ca88835926f70a9254e9341e28eb0
Prabhjyot <pgambhir@vcc.ca>
2025-03-15 16:48:40 -0700

    Login linked with Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { SignupPage } from "./pages/SignupPage";
 import { LoginPage } from "./pages/LoginPage";
+import { useEffect } from "react";
 
 function App() {
+
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isUserAuthenticated = JSON.parse(localStorage.getItem("user"));
+    if (!isUserAuthenticated) {
+      navigate("/login");
+    }
+  }, []);
 
   return (
     <>
       <Routes>
-        <Route element={<HomePage />} path="/home" />
+        <Route element={<HomePage />} path="/" />
         <Route element={<SignupPage />} path="/signup" />
-        <Route element={<LoginPage />} path="/" />
+        <Route element={<LoginPage />} path="/login" />
       </Routes>
     </>
   );
