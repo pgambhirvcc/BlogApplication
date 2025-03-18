0ae9efc1c2d059704f11878ae2911e383579430d
Prabhjyot Gambhir <pgambhir@vcc.ca>
2025-03-17 17:39:31 -0700

    Refactor Create Blogs UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,19 +13,12 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-const categories = ["Tech", "News", "Fashion", "Sports", "Food"];
+import CreateBlog from "./CreateBlog";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [createBlogInfo, setCreateBlogInfo] = useState({
-    title: "",
-    description: "",
-    image: "",
-    category: "",
-  });
 
   const navigateToHome = () => {
     navigate("/");
@@ -34,11 +27,6 @@ const Navbar = () => {
   const openDialogBox = () => {
     setIsDialogOpen(true);
   };
-
-  const handleOnSubmit = () => {
-    console.log(createBlogInfo);
-    navigate('/');
-  }
 
   return (
     <div>
@@ -60,71 +48,10 @@ const Navbar = () => {
             >
               Create Blog +
             </Button>
+
+            <CreateBlog isDialogOpen={isDialogOpen} setIsDialogOpen={setIsDialogOpen} />
+
           </Box>
-
-          <Dialog open={isDialogOpen}>
-            <DialogTitle>Create a blog</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                You can create a blog which will be visible to other users
-              </DialogContentText>
-
-              <Box display="flex" flexDirection="column" gap="20px">
-                <TextField
-                  placeholder="Enter blog title"
-                  value={createBlogInfo.title}
-                  onChange={(e) =>
-                    setCreateBlogInfo({
-                      ...createBlogInfo,
-                      title: e.target.value,
-                    })
-                  }
-                />
-
-                <TextField
-                  multiline
-                  placeholder="Enter blog description"
-                  value={createBlogInfo.description}
-                  onChange={(e) =>
-                    setCreateBlogInfo({
-                      ...createBlogInfo,
-                      description: e.target.value,
-                    })
-                  }
-                />
-
-                <TextField
-                  placeholder="Enter blog image url"
-                  value={createBlogInfo.image}
-                  onChange={(e) =>
-                    setCreateBlogInfo({
-                      ...createBlogInfo,
-                      image: e.target.value,
-                    })
-                  }
-                />
-
-                <Select
-                  value={createBlogInfo.category}
-                  label="Select Category"
-                  labelId="Select Category"
-                  onChange={(e) =>
-                    setCreateBlogInfo({
-                      ...createBlogInfo,
-                      category: e.target.value,
-                    })
-                  }
-                >
-                  {categories.map((category, index) => {
-                    return <MenuItem key={index} value={category}>{category}</MenuItem>;
-                  })}
-                </Select>
-
-
-                <Button variant="contained" onClick={handleOnSubmit}>Submit</Button>
-              </Box>
-            </DialogContent>
-          </Dialog>
         </Toolbar>
       </AppBar>
     </div>
