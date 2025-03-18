d48630de1cb18e97f436c2708e7ba8f8046f1263
Prabhjyot Gambhir <pgambhir@vcc.ca>
2025-03-17 17:28:22 -0700

    fixes #19 Create blog ui added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { SignupPage } from "./pages/SignupPage";
 import { LoginPage } from "./pages/LoginPage";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
 
 
   const navigate = useNavigate();
+  const [isUserLoggedIn, setUserLoggedIn] = useState(false);
 
   useEffect(() => {
     const isUserAuthenticated = JSON.parse(localStorage.getItem("user"));
+    setUserLoggedIn(isUserAuthenticated)
     if (!isUserAuthenticated) {
       navigate("/login");
     }
@@ -19,6 +22,7 @@ function App() {
 
   return (
     <>
+      { isUserLoggedIn ? <Navbar /> : null }
       <Routes>
         <Route element={<HomePage />} path="/" />
         <Route element={<SignupPage />} path="/signup" />
