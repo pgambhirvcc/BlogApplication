2c728ee93bf5fcfa87862eee66df3e8248e8dcee
Prabhjyot <pgambhir@vcc.ca>
2025-03-22 15:55:54 -0700

    fixes #29 delete blog added

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,6 +1,23 @@
-import { Card, CardContent, CardMedia, Chip, Typography } from "@mui/material";
+/* eslint-disable react/prop-types */
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Chip,
+  IconButton,
+  Snackbar,
+  Typography,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import { useState } from "react";
 
 const BlogCard = (props) => {
+  const [openNotification, setOpenNotification] = useState(false);
+
   const calculateCategoryColor = () => {
     switch (props.data.category) {
       case "Tech":
@@ -14,6 +31,18 @@ const BlogCard = (props) => {
     }
   };
 
+  const handleDeleteBlog = async () => {
+    try {
+      await deleteDoc(doc(db, "blogs", props.data.id));
+      setOpenNotification(true);
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Card>
       <CardMedia
@@ -23,9 +52,19 @@ const BlogCard = (props) => {
       />
 
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.data.title}
-        </Typography>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography gutterBottom variant="h5" component="div" margin="0">
+            {props.data.title}
+          </Typography>
+
+          <IconButton
+            aria-label="delete"
+            onClick={handleDeleteBlog}
+            color="error"
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Box>
 
         <Typography variant="body2" sx={{ color: "text.secondary" }}>
           {props.data.description}
@@ -39,10 +78,20 @@ const BlogCard = (props) => {
           <Chip label={props.data.category} color={calculateCategoryColor()} />
         </p>
 
-        <Typography variant="body2" fontWeight="bold" sx={{ color: "text.primary" }}>
+        <Typography
+          variant="body2"
+          fontWeight="bold"
+          sx={{ color: "text.primary" }}
+        >
           Created By: {props.data.author}
         </Typography>
       </CardContent>
+
+      <Snackbar
+        open={openNotification}
+        autoHideDuration={6000}
+        message={`Blog ${props.data.title} deleted successfully`}
+      />
     </Card>
   );
 };
