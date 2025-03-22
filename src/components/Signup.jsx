79c4713e4f3cde4ac737bdc3fb23476e17a1b3ca
Prabhjyot <pgambhir@vcc.ca>
2025-03-22 14:55:56 -0700

    fixes #36 added a snackbar

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,6 +3,7 @@ import {
   Button,
   Card,
   CardContent,
+  Snackbar,
   TextField,
   Typography,
 } from "@mui/material";
@@ -10,37 +11,46 @@ import React, { useEffect, useState } from "react";
 import LoginIcon from "@mui/icons-material/Login";
 import { validateInput } from "../utils";
 import { Link, useNavigate } from "react-router-dom";
-import { auth } from '../firebaseConfig';
+import { auth } from "../firebaseConfig";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export const Signup = () => {
   const [signupData, setSignupData] = useState({
     name: "",
     email: "",
-    password: ""
+    password: "",
   });
 
   const navigate = useNavigate();
 
   const [formDisabled, setFormDisabled] = useState(true);
+  const [openNotification, setOpenNotification] = useState(false);
 
   const handleSignup = async () => {
-   
-
-   try {
-    const user =  await createUserWithEmailAndPassword(auth, signupData.email, signupData.password);
-     if (user) {
-      alert('User Created Succesfully');
-      navigate('/login');
-     }
-   } catch (error) {
-      console.log(error, 'error');
-   }
-  }
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        signupData.email,
+        signupData.password
+      );
+      if (user) {
+        setOpenNotification(true);
+        setTimeout(() => {
+          navigate("/login");
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error, "error");
+    }
+  };
 
   // if all the fields are having a value, then return false other wise return true.
   useEffect(() => {
-    const isDisabled = validateInput([signupData.name, signupData.email, signupData.password]);
+    const isDisabled = validateInput([
+      signupData.name,
+      signupData.email,
+      signupData.password,
+    ]);
     setFormDisabled(isDisabled);
   }, [signupData]);
 
@@ -80,7 +90,12 @@ export const Signup = () => {
             }
           />
 
-          <Button variant="contained" color="primary" onClick={handleSignup} disabled={formDisabled}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSignup}
+            disabled={formDisabled}
+          >
             <LoginIcon />
             Signup
           </Button>
@@ -90,6 +105,12 @@ export const Signup = () => {
           </Box>
         </Box>
       </CardContent>
+
+      <Snackbar
+        open={openNotification}
+        autoHideDuration={6000}
+        message="Signup Completed Successfully"
+      />
     </Card>
   );
 };
